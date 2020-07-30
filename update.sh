5ee77ab6940aaec4c67245f9aa18c12d11bcadf6
thagki9 <thagki9@outlook.com>
2020-07-30 22:02:02 +0800

    Add workflows

---
+++ update.sh
@@ -0,0 +1,12 @@
+rm -rf Rules
+git clone -b master "https://thagki9:${GITHUB_TOKEN}@github.com/THaGKI9/ClashRules.git" Rules
+cd Rules
+git rm *
+
+cd ..
+python3 convert.py
+
+cd Rules
+git add -A
+git commit -m "Update rules on $(date -u)"
+git push origin master
